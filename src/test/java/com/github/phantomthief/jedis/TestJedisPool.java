9226ec5d7a8e4e03a403e1379f083061f5ee4a31
w.vela <boywang004@hotmail.com>
2017-01-13 14:56:24 +0900

    support generic type for jedis pool sub-class. (#5)

---
+++ src/test/java/com/github/phantomthief/jedis/TestJedisPool.java
@@ -0,0 +1,11 @@
+package com.github.phantomthief.jedis;
+
+import redis.clients.jedis.JedisPool;
+
+/**
+ * @author w.vela <wangtianzhou@kuaishou.com>
+ * Created on 2017-01-13.
+ */
+public class TestJedisPool extends JedisPool {
+
+}
